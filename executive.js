cfd43d2c0ee82c8a738979e1f1b976c63b16bc98
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-02-10 19:11:51 -0800

    Update executive.js
    HOst

--- executive.js
+++ executive.js
@@ -13,11 +13,11 @@ var padText = require('./padTable.js')
 
 // Link to mySQL Database
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     user: "root", //Your username
-    password: "Iforget123", //Your password
-    database: "Bamazon"
+    password: "", //Your password
+    database: "bamazon_db"
 });
 
 // Connect to Database
